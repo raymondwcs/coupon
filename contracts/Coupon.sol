05a28b7571093d373e17931549f1ba1532a7e05d
Raymond SO <raymond.wc.so@gmail.com>
2021-03-05 12:16:40 +0800

    updated

--- contracts/Coupon.sol
+++ contracts/Coupon.sol
@@ -5,35 +5,36 @@ pragma solidity ^0.6.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-////Import Auth from the access-control subdirectory
-////import "./access-control/Auth.sol";
-
 // Import Ownable from the OpenZeppelin Contracts library
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Coupon is Ownable, ERC721 {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
-    ////Auth private auth;
 
-    ////constructor(Auth _auth) public ERC721("Coupon", "CPN") {
-    constructor() public ERC721("Coupon", "CPN") {
-        ////auth = _auth;
-    }
+    mapping (uint256 => bool) public redeemed;
+
+    constructor() public ERC721("Coupon", "CPN") {}
 
     function awardCoupon(address customer, string memory tokenURI)
         public onlyOwner
         returns (uint256)
     {
-        ////Require that the caller is registered as an administrator in Auth
-        ////require(auth.isAdministrator(msg.sender), "Unauthorized");
-
         _tokenIds.increment();
 
         uint256 newCouponId = _tokenIds.current();
         _mint(customer, newCouponId);
         _setTokenURI(newCouponId, tokenURI);
 
+        redeemed[newCouponId] = false;
+
         return newCouponId;
+    }
+
+    function redeemCoupon(uint256 tokenId) public { 
+        require(ownerOf(tokenId) == msg.sender, "Not owner");
+        require(redeemed[tokenId] == false, "Redeemed already");
+
+        redeemed[tokenId] = true;
     }
 }
---
+++ test.js
@@ -0,0 +1,35 @@
+// scripts/index.js
+module.exports = async function main(callback) {
+    try {
+        // Our code will go here
+
+        const accounts = await web3.eth.getAccounts()
+        const Coupon = artifacts.require("Coupon")
+        const coupon = await Coupon.deployed()
+
+        console.log(`owner(): ${await coupon.owner()}`)
+        let results = await coupon.awardCoupon(accounts[0], "https://example.com/coupon/10", { from: accounts[0] })
+        console.log(`totalSupply(): ${await coupon.totalSupply()}`)
+
+        for (let i = 0; i < await coupon.totalSupply(); i++) {
+            let tokenId = await coupon.tokenByIndex(i)
+            console.log(`redeemed(${tokenId}) = ${await coupon.redeemed(tokenId)} `)
+        }
+
+        try {
+            await coupon.redeemCoupon(1)
+        } catch (error) {
+            console.log(error.message)
+        }
+
+        for (let i = 0; i < await coupon.totalSupply(); i++) {
+            let tokenId = await coupon.tokenByIndex(i)
+            console.log(`redeemed(${tokenId}) = ${await coupon.redeemed(tokenId)} `)
+        }
+
+        callback(0);
+    } catch (error) {
+        console.error(error);
+        callback(1);
+    }
+}
