fc093eed4753adecf2e20a6f9be423861a32abd2
Raymond SO <raymond.wc.so@gmail.com>
2021-03-14 11:38:31 +0800

    updated

--- src/App.js
+++ src/App.js
@@ -269,7 +269,7 @@ class Provider extends React.Component {
 class CouponSelector extends React.Component {
   render() {
     let couponItems = this.props.myCoupons.map(c =>
-      <div key={c.tokenId} className="d-flex p-2">
+      <div key={c.tokenId} className="d-flex p-1">
         {/* <div key={c.tokenId} className="col-sm-12 col-md-6 col-lg-6 mt-3"> */}
         <Card style={{ width: '18rem' }} bg={c.redeemed ? "light" : "black"}>
           <Card.Header as="h6">No. {c.tokenId}</Card.Header>
@@ -298,18 +298,15 @@ class CouponSelector extends React.Component {
                   </div>
               }
             </div>
-            {
-              !c.redeemed ?
-                <div class="d-flex flex-row-reverse align-self-end">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16" onClick={(e) => { alert(c.tokenURI) }} className="float-left">
-                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
-                    <path d="M8.93 6.588l-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
-                  </svg>
-                </div>
-                :
-                <div></div>
-            }
           </Card.Body>
+          <Card.Footer class="bg-transparent">
+            <div class="d-flex flex-row-reverse align-self-end mb-2 mr-2">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16" onClick={(e) => { alert(c.tokenURI) }} className="float-left">
+                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                <path d="M8.93 6.588l-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
+              </svg>
+            </div>
+          </Card.Footer>
         </Card>
       </div >
     )
