13f6e2a01624550228e998128a93a2f0c667a2e2
Raymond SO <raymond.wc.so@gmail.com>
2021-03-18 12:19:11 +0800

    scan blocks for events

---
+++ event.js
@@ -0,0 +1,49 @@
+// truffle exec --network ganache SimpleStorage
+
+module.exports = async function main(callback) {
+    try {
+        const accounts = await web3.eth.getAccounts()
+        const Contract = artifacts.require(process.argv[6])
+        const instance = await Contract.deployed()
+
+        let currentBlockNumber = await web3.eth.getBlockNumber()
+        console.log(`Current block number: ${currentBlockNumber}`)
+
+        // from block 0 to current block
+        for (let block = 0; block <= currentBlockNumber; block++) {
+            // get the numner of transaction in a block
+            console.log(`** Block [${block}]`)
+            let nTx = await web3.eth.getBlockTransactionCount(block)
+            console.log(`Number of transaction(s) in block [${block}]: ${nTx}`)
+            // get each transaction in a block
+            for (let i = 0; i < nTx; i++) {
+                let tx = await web3.eth.getTransactionFromBlock(block, i)
+                // console.log(tx)
+                let txReceipt = await web3.eth.getTransactionReceipt(tx.hash)
+                // console.log(txReceipt)
+                for (log of txReceipt.logs) {
+                    for (f of Contract.abi) {
+                        if (f.signature === log.topics[0]) {  // match `topics' against abi
+                            console.log(`Name: '${f.name}', type: ${f.type}`)
+                            try {
+                                var event = await web3.eth.abi.decodeLog(
+                                    f.inputs, log.data, log.topics.slice(1)
+                                )
+                                console.log(event)
+                            } catch (error) {
+                                console.error(error)
+                                console.log(f.inputs)
+                                console.log(log.data)
+                                console.log(log.topics)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        callback(0);
+    } catch (error) {
+        console.error(error);
+        callback(1);
+    }
+}
